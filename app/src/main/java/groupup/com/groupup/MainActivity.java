9fc26760ced7e57e92039bd54f305c1296b874e9
Qixiang Chen <chenq7@rpi.edu>
2018-10-03 12:57:40 -0400

    commit1

--- app/src/main/java/groupup/com/groupup/MainActivity.java
+++ app/src/main/java/groupup/com/groupup/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //comment
+        //comment hello 123
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
