cd773acae76b55871ebba313cccc442a7106ee11
StevenZuraski <zurass@rpi.edu>
2018-10-03 13:01:19 -0400

    i hate messages

--- app/src/main/java/groupup/com/groupup/MainActivity.java
+++ app/src/main/java/groupup/com/groupup/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         //comment hello 123
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     }
 }
