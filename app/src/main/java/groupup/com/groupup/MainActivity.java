7599281871cca984cf05a7c95a9a28320abefb81
Josh Beck <j.beck18@me.com>
2018-10-03 12:41:21 -0400

    updated comment

--- app/src/main/java/groupup/com/groupup/MainActivity.java
+++ app/src/main/java/groupup/com/groupup/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //abc123
+        //comment
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
