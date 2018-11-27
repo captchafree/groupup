1d724a1a0940ee84ca0a40b57b38b8163047087e
StevenZuraski <zurass@rpi.edu>
2018-11-27 10:21:12 -0500

    moved leavebutton to groupprofile and changed ui

---
+++ app/src/main/java/groupup/com/groupup/FinalCounter.java
@@ -0,0 +1,19 @@
+package groupup.com.groupup;
+
+public class FinalCounter {
+
+    private int val;
+
+    public FinalCounter(int intialVal) {
+        val = intialVal;
+    }
+    public void increment(){
+        val++;
+    }
+    public void decrement(){
+        val--;
+    }
+    public int getVal(){
+        return val;
+    }
+}
