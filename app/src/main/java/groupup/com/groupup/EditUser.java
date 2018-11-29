cde3a51a91b2c68d4279f5afff30ac3c82bad655
Nick Macneil <nickmac132@gmail.com>
2018-11-29 15:12:35 -0500

    location services working

--- app/src/main/java/groupup/com/groupup/EditUser.java
+++ app/src/main/java/groupup/com/groupup/EditUser.java
@@ -1,7 +1,9 @@
 package groupup.com.groupup;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +16,8 @@ import groupup.com.groupup.Authentication.Authenticator;
 import groupup.com.groupup.Database.DatabaseManager;
 import groupup.com.groupup.Database.GetDataListener;
 import groupup.com.groupup.Database.UserKeys;
+import groupup.com.groupup.LocationServices.LocationServiceManager;
+import groupup.com.groupup.LocationServices.Permissions;
 
 public class EditUser extends AppCompatActivity implements View.OnClickListener {
 
@@ -60,12 +64,34 @@ public class EditUser extends AppCompatActivity implements View.OnClickListener 
     }
 
     private void saveChanges() {
-        currentUser.setName(name.getText().toString().trim());
-        currentUser.setEmail(email.getText().toString().trim());
-        currentUser.setBio(bio.getText().toString().trim());
+        if(!Permissions.Check_FINE_LOCATION(this)) {
+            Permissions.Request_FINE_LOCATION(this,22);
+        }
 
-        DatabaseManager manager = DatabaseManager.getInstance();
-        manager.updateUserWithID(currentUser.getID(), currentUser);
+        if(!Permissions.Check_COARSE_LOCATION(this)) {
+            Permissions.Request_COARSE_LOCATION(this,22);
+        }
+
+        LocationServiceManager.LocationResult locationResult = new LocationServiceManager.LocationResult(){
+            @Override
+            public void gotLocation(Location location){
+
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                currentUser.setLatitude(latitude);
+                currentUser.setLongitude(longitude);
+
+                currentUser.setName(name.getText().toString().trim());
+                currentUser.setEmail(email.getText().toString().trim());
+                currentUser.setBio(bio.getText().toString().trim());
+
+                DatabaseManager manager = DatabaseManager.getInstance();
+                manager.updateUserWithID(currentUser.getID(), currentUser);
+            }
+        };
+        LocationServiceManager location = new LocationServiceManager();
+        location.getLocation(this, locationResult);
     }
 
     @Override
